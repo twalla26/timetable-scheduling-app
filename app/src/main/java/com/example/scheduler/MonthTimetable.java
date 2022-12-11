8b51465e34e272ba44605efbd582a4b4040f693a
grapyju <yoonju203@naver.com>
2022-12-11 18:13:59 +0900

    month_timetable_layout

---
+++ app/src/main/java/com/example/scheduler/MonthTimetable.java
@@ -0,0 +1,14 @@
+package com.example.scheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MonthTimetable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_month_timetable);
+    }
+}
