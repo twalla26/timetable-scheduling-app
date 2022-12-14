da772691e099e3c370655a8c0431731b9a46365c
grapyju <yoonju203@naver.com>
2022-12-14 18:01:01 +0900

    result_layout

---
+++ app/src/main/java/com/example/scheduler/ResultTime.java
@@ -0,0 +1,14 @@
+package com.example.scheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultTime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_time);
+    }
+}
